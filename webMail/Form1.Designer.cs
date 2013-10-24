6c89c3cd7b04e88dc7e826ad57b7087a7f660469
Michael Lotkowski <michael@lotkowscy.co.uk>
2013-10-24 16:53:06 +0100

    First Commit

---
+++ webMail/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace webMail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlLbl = new System.Windows.Forms.Label();
+            this.goBtn = new System.Windows.Forms.Button();
+            this.urlBox = new System.Windows.Forms.TextBox();
+            this.depthBox = new System.Windows.Forms.TextBox();
+            this.depthLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // urlLbl
+            // 
+            this.urlLbl.AutoSize = true;
+            this.urlLbl.Location = new System.Drawing.Point(12, 28);
+            this.urlLbl.Name = "urlLbl";
+            this.urlLbl.Size = new System.Drawing.Size(29, 13);
+            this.urlLbl.TabIndex = 0;
+            this.urlLbl.Text = "URL";
+            // 
+            // goBtn
+            // 
+            this.goBtn.Location = new System.Drawing.Point(207, 94);
+            this.goBtn.Name = "goBtn";
+            this.goBtn.Size = new System.Drawing.Size(75, 23);
+            this.goBtn.TabIndex = 1;
+            this.goBtn.Text = "GO";
+            this.goBtn.UseVisualStyleBackColor = true;
+            this.goBtn.Click += new System.EventHandler(this.goBtn_Click);
+            // 
+            // urlBox
+            // 
+            this.urlBox.Location = new System.Drawing.Point(53, 28);
+            this.urlBox.Name = "urlBox";
+            this.urlBox.Size = new System.Drawing.Size(418, 20);
+            this.urlBox.TabIndex = 2;
+            // 
+            // depthBox
+            // 
+            this.depthBox.Location = new System.Drawing.Point(53, 54);
+            this.depthBox.Name = "depthBox";
+            this.depthBox.Size = new System.Drawing.Size(28, 20);
+            this.depthBox.TabIndex = 4;
+            this.depthBox.Text = "0";
+            // 
+            // depthLbl
+            // 
+            this.depthLbl.AutoSize = true;
+            this.depthLbl.Location = new System.Drawing.Point(12, 54);
+            this.depthLbl.Name = "depthLbl";
+            this.depthLbl.Size = new System.Drawing.Size(36, 13);
+            this.depthLbl.TabIndex = 3;
+            this.depthLbl.Text = "Depth";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 129);
+            this.Controls.Add(this.depthBox);
+            this.Controls.Add(this.depthLbl);
+            this.Controls.Add(this.urlBox);
+            this.Controls.Add(this.goBtn);
+            this.Controls.Add(this.urlLbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label urlLbl;
+        private System.Windows.Forms.Button goBtn;
+        private System.Windows.Forms.TextBox urlBox;
+        private System.Windows.Forms.TextBox depthBox;
+        private System.Windows.Forms.Label depthLbl;
+    }
+}
+
---
+++ webMail/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace webMail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      public  struct websites
+        {
+            public string[] web;
+        }
+
+
+        string[] emails = new string[0];
+        int z = 0;
+        string[] web = new string[0];
+        int x = 0;
+        int depth = 0;
+        public void DownloadWebPage(string Url,ref websites newWeb)
+        {
+            // Open a connection
+            HttpWebRequest WebRequestObject = (HttpWebRequest)HttpWebRequest.Create(Url);
+            
+            // You can also specify additional header values like 
+            // the user agent or the referer:
+            WebRequestObject.UserAgent = ".NET Framework/2.0";
+            WebRequestObject.Referer = "http://www.example.com/";
+
+            // Request response:
+            WebResponse Response = WebRequestObject.GetResponse();
+
+            // Open data stream:
+            Stream WebStream = Response.GetResponseStream();
+
+            // Create reader object:
+            StreamReader Reader = new StreamReader(WebStream);
+
+            // Read the entire stream content:
+            
+            while (!Reader.EndOfStream)
+            {
+                string c = Reader.ReadLine();
+               string s= getMail(c);
+               if (s != "")
+               {
+                   Array.Resize<string>(ref emails, emails.Length + 1);
+                   emails[x] = s;
+                   x++;
+                  // MessageBox.Show(s);
+               }
+                string ss = getWeb(c);
+                if (ss != "")
+                {
+                    Array.Resize<string>(ref newWeb.web, newWeb.web.Length + 1);
+                    newWeb.web[z] = "http://" + ss;
+                   z++;
+                   // MessageBox.Show(ss);
+                }
+
+            }
+
+            // Cleanup
+            Reader.Close();
+            WebStream.Close();
+            Response.Close();
+
+          
+        }
+
+        public string getMail(string input)
+        {
+            Regex mail = new Regex(@"([a-zA-Z0-9]+@[a-zA-Z0-9]+)(\.[a-zA-Z0-9]*)+", RegexOptions.Compiled);
+            Match m = mail.Match(input);          
+            return  m.Groups[0].Value;
+        }
+
+        public string getWeb(string input)
+        {
+            Regex web = new Regex(@"http\://([a-zA-Z0-9\./\?\&=]*)", RegexOptions.Compiled);
+            Match m = web.Match(input);
+            return m.Groups[1].Value;
+        }
+        
+        private void reset()
+        {
+             emails = new string[0];
+            z = 0;
+             web = new string[0];
+            x = 0;
+             depth = 0;
+        }
+
+        private void goBtn_Click(object sender, EventArgs e)
+        {
+            reset();
+            depth = Convert.ToInt32(depthBox.Text);
+          websites[] toscan = new websites[depth+1];
+          for (int i = 0; i < toscan.Length; i++)
+          {
+              toscan[i].web = new string[1];
+          }
+              toscan[0].web = new string[1];
+               toscan[0].web[0]= urlBox.Text;
+            for (int i = 0; i <= depth; i++)
+            {
+                z = 0;
+                int v = toscan[i].web.Length;
+                for (int j = 0; j < v; j++)
+                {
+                    try
+                    { if(!(toscan[i].web[j].Contains(".exe"))&&!(toscan[i].web[j].Contains(".png"))&&!(toscan[i].web[j].Contains(".gif"))&&!(toscan[i].web[j].Contains(".flv"))&&!(toscan[i].web[j].Contains(".cab"))&&!(toscan[i].web[j].Contains(".swf")))
+                        DownloadWebPage(toscan[i].web[j], ref toscan[i+1]); }
+                    catch (Exception ex)
+                    { ; }
+                }
+            }
+            int webCount = 0;
+            string domain = urlBox.Text.Substring(7);
+            StreamWriter file = new StreamWriter(domain+"-webisites.txt");
+            for (int i = 0; i < toscan.Length; i++)
+            {
+                for (int j = 0; j < toscan[i].web.Length; j++)
+                {
+                    file.WriteLine(toscan[i].web[j]);
+                    webCount++;
+                }
+            }
+            file.Close();
+            int emailCount = 0;
+            StreamWriter file2 = new StreamWriter(domain+"-emails.txt");
+            if(emails.Length>0)
+            file2.Write(emails[0]);
+            for (int i = 1; i < emails.Length; i++)
+            {
+                file2.Write("," + emails[i]);
+                emailCount++;
+            }
+            file2.Close();
+            MessageBox.Show("Done! Scanned " + webCount.ToString() + " websites; got " + emailCount.ToString() + " emails");
+        }
+    }
+}
